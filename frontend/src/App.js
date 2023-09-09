e5216c2e43f3c96594575448e5f52e274ab67371
lowqwroby <119352726+lowqwroby@users.noreply.github.com>
2023-09-10 00:23:31 +0300

    make header and main changes in react-project

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,10 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
+
+import './scss/app.scss'; // Подключение стилей 
+
+import Header from './components/Header';
 
 const axiosClient = axios.create({
   baseURL: `http://pro-learn`,
@@ -31,22 +35,7 @@ const App = () => {
   
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Header />
   );
 }
 
