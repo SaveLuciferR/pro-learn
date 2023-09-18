60958ac6bd2905c7b75d7317cedf9c0a71fe162d
Дарья Мокеева <eltiobvs@gmail.com>
2023-09-18 14:20:00 +0300

    outputting information from the backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,63 +1,19 @@
 // import logo from './logo.svg';
 import "./App.css";
-import axios from "axios";
-import React, {useState, useEffect} from "react";
 
 import "./scss/app.scss"; // Подключение стилей
 
 import Header from "./components/Header";
 import Sidebar from "./components/Sidebar";
-import AllArticlesMain from "./components/All_Articles/AllArticlesMain";
-import TestGet from "./axios";
-
-const axiosClient = axios.create({
-  baseURL: `http://pro-learn`,
-});
+import BlogPage from "./pages/BlogPage";
 
 const App = () => {
-  
-  const [articles, setArticles] = useState([]);
-  
-  useEffect(() => {
-    axiosClient
-      .get(axiosClient.baseURL)
-      .then(data => {
-        setArticles(data.allBlogs);
-      })
-  }, []);
-
-  function testGet() {
-    axiosClient
-      .post("/blog/hello-world")
-      .then(({ data }) => {
-        // console.log(data);
-        // console.log(data.allBlogs);
-
-        // data.allBlogs.map(element => {
-        //   if (element.popular == 1) console.log(element);
-        // })
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  testGet();
-const datatest = () => {
-  
-}
 
   return (
     <div>
-      <TestGet/>
-      {/* {articles.map(article => {
-        return (
-          
-        )
-      })} */}
-      {/* <Header />
+      <Header />
       <Sidebar />
-      <AllArticlesMain /> */}
+      <BlogPage />
     </div>
   );
 };
